3d472d9df45eb148cf31d414dc8c3bfb8965731e
Kushagra <kushagragupta6038@gmail.com>
2020-12-19 16:54:04 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Hello Cleaver Programmers, Let's build amazone store </h1>
+    
+    {/* Header */}
+    <Header />
+
+    {/* Home */}
+   </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAkmT5wYghBxT5onWosIx28fRqd2TV9Zzc",
+    authDomain: "clone-f6475.firebaseapp.com",
+    projectId: "clone-f6475",
+    storageBucket: "clone-f6475.appspot.com",
+    messagingSenderId: "532360013983",
+    appId: "1:532360013983:web:ee4d4c2ea01d7d68c65aaa",
+    measurementId: "G-Q62GV04R8Y"
+  };
